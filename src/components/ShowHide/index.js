54a230248331505ae7bf65b674bbb2c06358bbed
chinnaragamsetti <110217256+chinnaragamsetti@users.noreply.github.com>
2022-09-16 16:56:33 +0530

    Update index.js

--- src/components/ShowHide/index.js
+++ src/components/ShowHide/index.js
@@ -1,51 +1,46 @@
 // Write your code here
-import {Component} from "react"
+import {Component} from 'react'
 import './index.css'
 
 class ShowHide extends Component {
-    state={firstname:true,lastname:true}
-    firstname=()=>{
-        const {firstname}=this.state;
-        if (this.firstname===true){
-            return(
-                <div className="namecontianer">
-            <h1 className="h2">Joe</h1>
-          </div>
-            )
-        }
-
-    }
-    lastname=()=>{
-        const {lastname}=this.state;
-        if (this.lastname===true){
-            return(
-                <div className="namecontianer">
-            <h1 className="h2">henry</h1>
-          </div>
-        </div>
-            )
-        }
-
-    }
+  state = {isfirstname: false, lastname: false}
+  firstname = () => {
+    this.setState((isfirstname: true))
+  }
+  lastname = () => {
+    this.setState((islastname: true))
+  }
 
   render() {
-    return(
+    const {isfirstname, islastname} = state
+    return (
       <div className="bg">
         <h1 className="h">Show/Hide</h1>
         <div className="fullnamecontainer">
-            <div class="firstnamecontainer">
-                    <button className="butt" onClick={this.firstname}>Show/Hide Firstname</button>
-                    {this.firstname()}
-            </div>
-            <div className="lastnamecontainer">
-            
-                     <button className="butt" onClick={this.lastname}>Show/Hide Lastname</button>
-                     {this.lastname()}
-            </div>
-          
-          
+          <div class="firstnamecontainer">
+            <button className="butt" onClick={this.firstname} type="button">
+              Show/Hide Firstname
+            </button>
+            {isfirstname ? (
+              <div className="namecontianer">
+                <h1 className="h2">Joe</h1>
+              </div>
+            ) : null}
+          </div>
+          <div className="lastnamecontainer">
+            <button className="butt" onClick={this.lastname} type="button">
+              Show/Hide Lastname
+            </button>
+            {islastname ? (
+              <div className="namecontianer">
+                <h1 className="h2">Jonas</h1>
+              </div>
+            ) : null}
+          </div>
+        </div>
       </div>
     )
   }
 }
+
 export default ShowHide
