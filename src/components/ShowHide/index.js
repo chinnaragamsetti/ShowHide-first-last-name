1c484733f4cca5bd5472b9f78c6c6888aef2af09
chinnaragamsetti <110217256+chinnaragamsetti@users.noreply.github.com>
2022-09-20 21:13:02 +0530

    Update index.js

--- src/components/ShowHide/index.js
+++ src/components/ShowHide/index.js
@@ -1,41 +1,46 @@
 // Write your code here
+// Write your code here
 import {Component} from 'react'
 import './index.css'
 
 class ShowHide extends Component {
-  state = {isfirstname: false, lastname: false}
-  firstname = () => {
-    this.setState(prevState=>({isfirstname:!prevState.isfirstname)
+  state = {isFirstname: false, islastName: false}
+  firstName = () => {
+    this.setState(prevState=>({isFirstname: !prevState.isFirstname)
   }
-  lastname = () => {
-    this.setState(prevState=>({islastname:!prevState.islastname)
+  lastnName = () => {
+    this.setState(prevState=>({isLastname: !prevState.isLastname)
   }
 
+  onFirstname=()=>{
+      <div className="namecontianer">
+                <h1 className="h2">Joe</h1>
+              </div>
+  }
+  onlastName=()=>{
+      <div className="namecontianer">
+                <h1 className="h2">Jonas</h1>
+              </div>
+  }
+
+
   render() {
-    const {isfirstname, islastname} = state
+    const {isFirstname, isLastname} = state
     return (
       <div className="bg">
         <h1 className="h">Show/Hide</h1>
         <div className="fullnamecontainer">
           <div class="firstnamecontainer">
-            <button className="butt" onClick={this.firstname} type="button">
+            <button className="butt" onClick={this.firstName} type="button">
               Show/Hide Firstname
             </button>
-            {isfirstname ? (
-              <div className="namecontianer">
-                <h1 className="h2">Joe</h1>
-              </div>
-            ) : null}
+            {isfirstName ? this.onFirstname(): null}
           </div>
           <div className="lastnamecontainer">
-            <button className="butt" onClick={this.lastname} type="button">
+            <button className="butt" onClick={this.lastName} type="button">
               Show/Hide Lastname
             </button>
-            {islastname ? (
-              <div className="namecontianer">
-                <h1 className="h2">Jonas</h1>
-              </div>
-            ) : null}
+            {isLastname ? this.onlastName(): null}
           </div>
         </div>
       </div>
